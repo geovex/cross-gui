d8750ffe6025b2367eb041b9b11506f182348a08
Heorhi Valakhanovich <valahanovich@tut.by>
2017-03-23 23:02:00 +0300

    draw gtk window

--- src/pal/unix/mod.rs
+++ src/pal/unix/mod.rs
@@ -1,21 +1,30 @@
 use gtk;
 use std::marker::PhantomData;
 use ::gui::Gui;
+use std::sync::{Once, ONCE_INIT};
+
+static INIT: Once = ONCE_INIT;
 
 pub struct PalGui<M> {
     phantom: PhantomData<M>
 }
 
 impl<M> Gui for PalGui<M> {
-    type Window = ();
+    type Window = gtk::Window;
     type Msg = M;
     fn new() -> PalGui<M> {
+        INIT.call_once(|| gtk::init().unwrap());
         PalGui {
             phantom: PhantomData
         }
     }
-    fn new_window(&mut self) -> () {
-        unimplemented!()
+    fn new_window(&mut self) -> gtk::Window {
+        use gtk::WidgetExt;
+        use gtk::WindowExt;
+        let window = gtk::Window::new(gtk::WindowType::Toplevel);
+        window.set_default_size(400,400);
+        window.show_all();
+        window
     }
     fn event_loop(&mut self) {
         gtk::main();
